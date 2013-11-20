b8d5c7e11453be467490580485c313a867cab00e
Ludovic Viaud <ludovic.viaud@gmail.com>
2013-11-20 15:11:12 +0100

    test avancé

--- discret.py
+++ discret.py
@@ -2,30 +2,30 @@
 module permetant d'arrondir selon plusieur mode et plusieurs precisions
 """
 
+import math
+
 def discret(value, precision=1, mode="auto") :
-    if precision == 0 : return value
-    if value == 0 : return 0
-    value = float(value)
+    if precision not in [1, 2, 4, 5, 8, 10]:
+        raise ValueError('precision doit etre 1, 2, 4, 5, 8 ou 10')
+    if mode not in ['auto', 'trunc', '-trunc', 'floor', 'ceil']:
+        raise ValueError('mode doit etre "auto", "tronc", "-tronc", "floor", "ceil"')
+    if precision == 0 :
+        return value
+    if value == 0 :
+        return 0
     if mode == 'auto' :
-        temp = value/precision
-        tempint = int(temp)
-        result = (tempint + int(round(temp-tempint)))*precision
+        result = round(value*precision)/precision
     elif mode == "trunc" :
-        result = int(value/precision)*precision
+        result = int(value*precision)/precision
     elif mode == "-trunc" :
-        result = int(value/precision)*precision
-        result += result >= 0 and precision or -precision
+        temp = int(value*precision)
+        temp += temp >= 0 and 1 or -1
+        result = temp/precision
     elif mode == "floor" :
-        result = int(value/precision)*precision
-        if result < 0 :
-            result -= precision
+        result = math.floor(value*precision)/precision
     elif mode == "ceil" :
-        result = int(value/precision)*precision
-        if result >= 0 :
-            result += precision
-    else :
-        raise Exception('%s is not a valide mode' % mode)
-    
+        result = math.ceil(value*precision)/precision
+
     return result
 
 if __name__ == "__main__" :
---
+++ test/testperf_discret.py
@@ -0,0 +1,5 @@
+'''
+Created on 20 nov. 2013
+
+@author: Ludo
+'''
